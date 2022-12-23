2cdea3b4e49a86ce0ab1a23aabfbe2248b9bfc6e
Aron <59264146+AronMikes@users.noreply.github.com>
2022-12-23 21:13:51 +0100

    ckeditor added, cards added

--- base/forms.py
+++ base/forms.py
@@ -5,7 +5,7 @@ from .models import Room, Message
 class RoomForm(ModelForm):
     class Meta:
         model = Room
-        fields = '__all__'
+        fields = ['topic', 'name', 'description']
 
 class MessageForm(ModelForm):
     class Meta:
